0833c656c0434e2767656492a7d55c3a79044f33
JiangNing <jinniudashu@gmail.com>
2022-05-03 21:03:02 +0800

    Added is_system_service into Service

--- define_operand/admin.py
+++ define_operand/admin.py
@@ -56,7 +56,7 @@ class ServiceAdmin(admin.ModelAdmin):
     list_display_links = ['label', 'name',]
     fieldsets = (
         ('基本信息', {
-            'fields': (('label', 'name_icpc'), ('managed_entity', 'priority'), 'role', ('history_services_display', 'enable_queue_counter'), ('name', 'hssc_id'))
+            'fields': (('label', 'name_icpc'), ('managed_entity', 'priority', 'is_system_service'), 'role', ('history_services_display', 'enable_queue_counter'), ('name', 'hssc_id'))
         }),
         ('作业管理', {
             'fields': ('suppliers', 'not_suitable', ('awaiting_time_frame' ,'execution_time_frame'), 'working_hours', 'cost', 'load_feedback')
