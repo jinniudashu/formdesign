88765e75581e2f4072f7c97ed32e73d9263d77fe
JiangNing <jinniudashu@gmail.com>
2023-03-25 11:55:17 +0800

    FiledsType增加虚拟职员类型定义： priority_operator = "core.VirtualStaff"

--- define_backup/export_source_code.py
+++ define_backup/export_source_code.py
@@ -140,7 +140,8 @@ class FieldsType(Enum):
     overtime = "Datetime"  # 超期时限
     scheduled_operator = "entities.Stuff"  # 计划执行人员
     service = "core.Service"  # 服务
-    is_assigned = 'Boolean'  # 是否已生成任务
+    priority_operator = "core.VirtualStaff"  # 虚拟职员
+    is_assigned = "Boolean"  # 是否已生成任务
 
     # 自动生成字段数据类型'''
 
