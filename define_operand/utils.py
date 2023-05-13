43d63e01da651cbe57e87ab9e68f1197bf8d3ffd
jinniudashu <jinniudashu@gmail.com>
2023-05-13 22:21:19 +0800

    Modified generate_js_script(); Added generate_form_assistance_prompt();

--- define_operand/utils.py
+++ define_operand/utils.py
@@ -7,13 +7,16 @@ def generate_js_script(params):
 
     openai.api_key = OPENAI_API_KEY
 
-    messages=[]
+    messages = None
+
     # prompt = {'form_events': form_events, 'computed_fields': ''}  # 提示信息清单
     # 根据prompt调用不同的提示生成脚本
     if params.get('form_events'):
-        messages = generate_form_assistance_prompt(params.get('form_events'))
-        return messages
-    elif params.get('computed_fields'):
+        form_events = params.get('form_events')
+        form_event = form_events[0]
+        messages = generate_form_assistance_prompt(form_event['expression'])
+    
+    if params.get('computed_fields'):
         messages = generate_computed_fields_prompt(params.get('computed_fields'))
 
     response = openai.ChatCompletion.create(
@@ -28,7 +31,63 @@ def generate_js_script(params):
 
 
 def generate_form_assistance_prompt(param):
-    return f"hi, your script is generated here ...{param}"
+    SYSTEM_PROMPT = 'You are a seasoned Javascript developer.'
+
+    USER_INPUT_PREFIX = '''Your task is to write a piece of JavaScript code, but before that, please read a python logic expression to refer to its business logic. When this expression is true, it means that a business event has occurred: the values of the fields involved are in conflict.
+After you understand this point, please write a concise JavaScript code that uses the same logic rules to monitor whether the corresponding field values ​​involved in a web page form matches with the rules. If so, an alert pops up to warn the user: the relevant fields value has a conflicting.
+The specific requirements for the implementation code are as follows:
+Use the 'document. addEventListener' method to listen for the DOMContentLoaded event and execute a callback function when the page is loaded.
+The callback function should include the following steps:
+1. Use document.getElementById to retrieve the DOM elements of all fields involved in the given python expression in angle brackets. Follow this naming convention: the field name prefixed with "id_" is the field ID.
+2. Write a function named as "form_event" to implement the event expression logic, which accepts the fields values as arguments, and returns a boolean. Use the same logic given in the python expression.
+3. Write an update function named as "update_input", evaluate if the related fields value matches the form event condition when user inputs into related field. If so, alert the user. The format of the warning message is "<value1> conflicts with <value2>!"
+4. Add input event listeners to the DOM elements of related fields involved above, used "update_input" as callback.
+5. This code will be injected into a web page, so please put the code in the <script></script> tag.
+Follow the required steps above and the example below, step by step.
+The Python expression in angle brackets are used to illustrate business logic: < '''
+
+    EXAMPLE_USER_INPUT = '''len({'糖尿病'}.intersection(boolfield_zhen_duan)) == 1 and len({'葡萄糖'}.intersection(boolfield_yao_pin_ming_cheng)) == 1'''
+
+    EXAMPLE_USER_INPUT = USER_INPUT_PREFIX + EXAMPLE_USER_INPUT + " >"
+
+    EXAMPLE_ASSISTANT_RESPONSE = '''<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Retrieve DOM elements of all fields involved in the expression logic.
+        const boolfield_zhen_duan = document.getElementById("id_boolfield_zhen_duan");
+        const boolfield_yao_pin_ming_cheng = document.getElementById("id_boolfield_yao_pin_ming_cheng");
+
+        // A function implementing the event expression logic. It accepts the field values as arguments, and returns a boolean
+        const form_event = (zhen_duan, yao_pin_ming_cheng) => {
+            return new Set(['糖尿病']).has(boolfield_zhen_duan) && new Set(['葡萄糖']).has(boolfield_yao_pin_ming_cheng);
+        };
+
+        // Evaluate if the related fields value matches the form event condition, if so, alert the user
+        const update_input = () => {
+            const zhen_duan = boolfield_zhen_duan.value;
+            const yao_pin_ming_cheng = boolfield_yao_pin_ming_cheng.value;
+            form_event_value = form_event(zhen_duan, yao_pin_ming_cheng);
+            // If form_event() returns true, then alert the user that the valeus are conflicting
+            if (form_event_value) {
+                alert("<糖尿病>与<葡萄糖>冲突");
+            }
+        };
+
+        // Add input event listener to related fields
+        boolfield_zhen_duan.addEventListener("input", update_input);
+        boolfield_yao_pin_ming_cheng.addEventListener("input", update_input);
+    });
+</script>'''
+
+    prompt = USER_INPUT_PREFIX + param + " >"
+
+    messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": EXAMPLE_USER_INPUT},
+            {"role": "assistant", "content": EXAMPLE_ASSISTANT_RESPONSE},
+            {"role": "user", "content": prompt}
+        ]
+
+    return messages
 
 
 def generate_computed_fields_prompt(param):
