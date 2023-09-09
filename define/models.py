affa30f4addc86ffeead9458b65e5e806e2af0fd
JiangNing <jinniudashu@gmail.com>
2023-09-09 15:42:17 +0800

    系统字段更名为系统API字段

--- define/models.py
+++ define/models.py
@@ -111,11 +111,12 @@ class FileField(HsscFieldBase):
 # 系统字段
 class SystemReservedField(HsscFieldBase):
     FIELD_TYPE = [('charge_staff', '责任人'), ('operator', '作业人员'), ('scheduled_time', '计划执行时间'), ('duration', '时长'), ('frequency', '频次')]
+    # FIELD_TYPE = [('hssc_charge_staff', '责任人'), ('hssc_operator', '作业人员'), ('hssc_scheduled_time', '计划执行时间'), ('hssc_duration', '时长'), ('hssc_frequency', '频次')]
     type = models.CharField(max_length=50, choices=FIELD_TYPE, default='ImageField', verbose_name="类型")
 
     class Meta:
-        verbose_name = "系统字段"
-        verbose_name_plural = "系统字段"
+        verbose_name = "系统API字段"
+        verbose_name_plural = "系统API字段"
 
 # 字段列表
 class Component(HsscPymBase):
