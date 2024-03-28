35fdf78362ce2f5db532e6d995ba0fe48ea67574
JiangNing <jinniudashu@gmail.com>
2024-03-28 11:45:32 +0800

    Added receive_data_from into ServiceRule

--- define_operand/models.py
+++ define_operand/models.py
@@ -1056,6 +1056,7 @@ class ServiceRule(HsscBase):
     event_rule = models.ForeignKey(EventRule, on_delete=models.CASCADE,  blank=True, null=True, verbose_name='条件事件')
     system_operand = models.ForeignKey(SystemOperand, on_delete=models.CASCADE, blank=True, null=True, verbose_name='系统作业')
     next_service = models.ForeignKey(Service, on_delete=models.CASCADE, blank=True, null=True, related_name='next_service', verbose_name='后续服务')
+    receive_data_from = models.ManyToManyField(Service, blank=True, related_name='receive_data_from', verbose_name='接收数据')
     Receive_form = [(0, '否'), (1, '接收，不可编辑'), (2, '接收，可以编辑')]  # 接收表单数据
     passing_data = models.PositiveSmallIntegerField(choices=Receive_form, default=0,  blank=True, null=True, verbose_name='接收表单')
     apply_to_group = models.BooleanField(choices=[(False, '否'), (True, '是')], default=False, verbose_name='使用分组')
